53491481d1faae69d50b95f806d4a6f2b703eba5
NETPAY\nyaz.jaff <nyaz.jaff@technologi.co.uk>
2019-05-24 13:19:40 +0100

    New learning project HelloA

---
+++ helloA/src/main/java/my/hello/world/demo/GreetingController.java
@@ -0,0 +1,17 @@
+package my.hello.world.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="MYNAM IS MUA") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
---
+++ helloA/src/main/java/my/hello/world/demo/HelloworldAApplication.java
@@ -0,0 +1,13 @@
+package my.hello.world.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldAApplication.class, args);
+	}
+
+}
