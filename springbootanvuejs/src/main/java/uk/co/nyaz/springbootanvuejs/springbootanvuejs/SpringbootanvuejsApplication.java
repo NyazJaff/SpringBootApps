f9158c72bac9f5e3600ae6a9417858edd653864a
Nyaz Jaff <n.jaff@livelinktechnology.net>
2020-07-08 11:58:17 +0100

    unwanted projects deleted

---
+++ springbootanvuejs/src/main/java/uk/co/nyaz/springbootanvuejs/springbootanvuejs/SpringbootanvuejsApplication.java
@@ -0,0 +1,13 @@
+package uk.co.nyaz.springbootanvuejs.springbootanvuejs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootanvuejsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootanvuejsApplication.class, args);
+	}
+
+}
---
+++ springbootanvuejs/src/test/java/uk/co/nyaz/springbootanvuejs/springbootanvuejs/SpringbootanvuejsApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.nyaz.springbootanvuejs.springbootanvuejs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootanvuejsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
